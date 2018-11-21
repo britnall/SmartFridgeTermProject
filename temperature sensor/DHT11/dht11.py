a6ef95bede7f03cc5796462f510793ba0c0216b4
Brittany Nall <brittany.nall@gmail.com>
2018-11-21 10:07:25 -0700

    update sensor code

--- temperature sensor/DHT11/dht11.py
+++ temperature sensor/DHT11/dht11.py
@@ -1,5 +1,5 @@
 import time
-import RPi
+import RPi.GPIO as GPIO
 
 
 class DHT11Result:
@@ -31,16 +31,16 @@ class DHT11:
         self.__pin = pin
 
     def read(self):
-        RPi.GPIO.setup(self.__pin, RPi.GPIO.OUT)
+        GPIO.setup(self.__pin, GPIO.OUT)
 
         # send initial high
-        self.__send_and_sleep(RPi.GPIO.HIGH, 0.05)
+        self.__send_and_sleep(GPIO.HIGH, 0.05)
 
         # pull down to low
-        self.__send_and_sleep(RPi.GPIO.LOW, 0.02)
+        self.__send_and_sleep(GPIO.LOW, 0.02)
 
         # change to input using pull up
-        RPi.GPIO.setup(self.__pin, RPi.GPIO.IN, RPi.GPIO.PUD_UP)
+        GPIO.setup(self.__pin, GPIO.IN, GPIO.PUD_UP)
 
         # collect data into an array
         data = self.__collect_input()
@@ -67,10 +67,11 @@ class DHT11:
         return DHT11Result(DHT11Result.ERR_NO_ERROR, the_bytes[2], the_bytes[0])
 
     def __send_and_sleep(self, output, sleep):
-        RPi.GPIO.output(self.__pin, output)
+        GPIO.output(self.__pin, output)
         time.sleep(sleep)
 
     def __collect_input(self):
+
         # collect the data while unchanged found
         unchanged_count = 0
 
@@ -80,7 +81,7 @@ class DHT11:
         last = -1
         data = []
         while True:
-            current = RPi.GPIO.input(self.__pin)
+            current = GPIO.input(self.__pin)
             data.append(current)
             if last != current:
                 unchanged_count = 0
@@ -93,6 +94,7 @@ class DHT11:
         return data
 
     def __parse_data_pull_up_lengths(self, data):
+
         STATE_INIT_PULL_DOWN = 1
         STATE_INIT_PULL_UP = 2
         STATE_DATA_FIRST_PULL_DOWN = 3
@@ -101,8 +103,8 @@ class DHT11:
 
         state = STATE_INIT_PULL_DOWN
 
-        lengths = [] # will contain the lengths of data pull up periods
-        current_length = 0 # will contain the length of the previous period
+        lengths = []  # will contain the lengths of data pull up periods
+        current_length = 0  # will contain the length of the previous period
 
         for i in range(len(data)):
 
@@ -110,36 +112,40 @@ class DHT11:
             current_length += 1
 
             if state == STATE_INIT_PULL_DOWN:
-                if current == RPi.GPIO.LOW:
+                if current == GPIO.LOW:
                     # ok, we got the initial pull down
                     state = STATE_INIT_PULL_UP
                     continue
                 else:
                     continue
+
             if state == STATE_INIT_PULL_UP:
-                if current == RPi.GPIO.HIGH:
+                if current == GPIO.HIGH:
                     # ok, we got the initial pull up
                     state = STATE_DATA_FIRST_PULL_DOWN
                     continue
                 else:
                     continue
+
             if state == STATE_DATA_FIRST_PULL_DOWN:
-                if current == RPi.GPIO.LOW:
+                if current == GPIO.LOW:
                     # we have the initial pull down, the next will be the data pull up
                     state = STATE_DATA_PULL_UP
                     continue
                 else:
                     continue
+
             if state == STATE_DATA_PULL_UP:
-                if current == RPi.GPIO.HIGH:
+                if current == GPIO.HIGH:
                     # data pulled up, the length of this pull up will determine whether it is 0 or 1
                     current_length = 0
                     state = STATE_DATA_PULL_DOWN
                     continue
                 else:
                     continue
+
             if state == STATE_DATA_PULL_DOWN:
-                if current == RPi.GPIO.LOW:
+                if current == GPIO.LOW:
                     # pulled down, we store the length of the previous pull up period
                     lengths.append(current_length)
                     state = STATE_DATA_PULL_UP
@@ -150,39 +156,48 @@ class DHT11:
         return lengths
 
     def __calculate_bits(self, pull_up_lengths):
+
         # find shortest and longest period
         shortest_pull_up = 1000
         longest_pull_up = 0
 
         for i in range(0, len(pull_up_lengths)):
+
             length = pull_up_lengths[i]
             if length < shortest_pull_up:
                 shortest_pull_up = length
+
             if length > longest_pull_up:
                 longest_pull_up = length
 
         # use the halfway to determine whether the period it is long or short
         halfway = shortest_pull_up + (longest_pull_up - shortest_pull_up) / 2
+
         bits = []
 
         for i in range(0, len(pull_up_lengths)):
+
             bit = False
             if pull_up_lengths[i] > halfway:
                 bit = True
+
             bits.append(bit)
 
         return bits
 
     def __bits_to_bytes(self, bits):
+
         the_bytes = []
         byte = 0
 
         for i in range(0, len(bits)):
+
             byte = byte << 1
             if (bits[i]):
                 byte = byte | 1
             else:
                 byte = byte | 0
+
             if ((i + 1) % 8 == 0):
                 the_bytes.append(byte)
                 byte = 0
--- temperature sensor/temp_sensing.py
+++ temperature sensor/temp_sensing.py
@@ -1,35 +1,52 @@
-import RPi.GPIO as GPIO
 from DHT11 import dht11
+import RPi.GPIO as GPIO
 import time
 import datetime
 from alert import send_gmail_alert
 
-# initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
+data_pin=14
 
-# read data using pin 14
-instance = dht11.DHT11(pin=14)
 
-# Only send email alert once every hour
-last_email_time = datetime.datetime.now()
+def main():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
 
-while True:
-    result = instance.read()
-    if result.is_valid():
-        print("Last valid input: " + str(datetime.datetime.now()))
+    # read data using pin 14
+    instance = dht11.DHT11(pin=data_pin)
+
+    last_email_time = datetime.datetime.now()
+    first_update = True
+
+    while True:
+        result = instance.read()
+
         print("Temperature: %d C" % result.temperature)
         print("Humidity: %d %%" % result.humidity)
 
-        temp_fehrenheit = (float(result.temperature) * (9.0/5.0)) + 32.0
-        current_time = datetime.datetime.now()
+        if result.is_valid():
+            current_time = datetime.datetime.now()
+            print("Last valid input: " + str(current_time))
 
-        # FDA regulation at or below 40 degrees is unsafe for refrigeration
-        if not temp_fehrenheit <= 40.0 and not temp_fehrenheit > 0.0:
-            if last_email_time.hour - current_time.hour >= 1:
-                send_gmail_alert(temp_fehrenheit, current_time)
-                last_email_time = current_time
+            temp_fehrenheit = (float(result.temperature) * (9.0/5.0)) + 32.0
+            # FDA regulation at or below 40 degrees is unsafe for refrigeration
+            if not (temp_fehrenheit <= 40.0 and temp_fehrenheit > 0.0):
 
-        # check temperature every 5 minutes if last value was valid
-        time.sleep(60*5)
+                # Only send email alert once every hour
+                elapsed_time = current_time - last_email_time
+                if elapsed_time > datetime.timedelta(hours=1) or first_update:
+                    print("GMAIL ALERT SENT")
+                    send_gmail_alert(temp_fehrenheit, current_time)
+                    first_update = False
+                    last_email_time = current_time
+
+            # check temperature every minute if last value was valid
+            time.sleep(60)
+        else:
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Program Killed.")
